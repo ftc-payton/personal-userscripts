4720c41b40e019c9ce6051bcd190792c53286b94
ftc-payton <paytonbrian1126@gmail.com>
2024-08-05 18:03:57 -0400

    add open-twitter

---
+++ open-twitter.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Replace HTTPS with Custom Protocol
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Replaces https:// with myapp:// to open an app
+// @author       Your Name
+// @match        https://x.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+
+    let currentUrl = window.location.href;
+
+    if (currentUrl.includes('/status/')) {
+        let parts = currentUrl.split('/');
+        let id = parts.pop();
+        let newUrl = 'twitter://status/?id=' + id;
+        window.location.href = newUrl;
+    }
+})();
